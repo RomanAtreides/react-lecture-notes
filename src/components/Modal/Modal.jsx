210738e4f04fe652545d3dba2bb41aaebbf47f83
Roman Nazarenko <justaspeckinspace@gmail.com>
2026-01-05 21:15:11 +0300

    Fix note modal window

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,7 @@
 import styles from "./Modal.module.css";
 
 export const Modal = ({ isOpen, onClose, children }) => {
-    if (isOpen) {
+    if (!isOpen) {
         return null;
     }
     return (
