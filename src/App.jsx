ca0045c8de44e99919897766b5f61ccd10b5707a
Roman Nazarenko <justaspeckinspace@gmail.com>
2026-01-06 13:26:32 +0300

    Add Notes component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useCallback, useContext, useEffect, useMemo, useState } from "react";
 import styles from "./App.module.css";
 import { Header } from "./components/Header/Header";
 import { NotesList } from "./components/NotesList/NotesList";
@@ -10,34 +10,50 @@ const LOCAL_STORAGE_KEY = "notes";
 
 const NotesContext = createContext();
 
+function Notes() {
+    const { notes, setNotes } = useContext(NotesContext);
+    const [searchQuery, setSearchQuery] = useState("");
+    const filteredNotes = notes.filter(
+        (note) => note.title.toLowerCase().includes(searchQuery.toLowerCase()) || note.tags.includes(searchQuery)
+    );
+    const uniqueTags = useMemo(() => [...new Set(notes.reduce((acc, note) => acc.concat(note.tags), []))], [notes]);
+
+    const onTagClick = useCallback(
+        (tag) => {
+            setSearchQuery(tag);
+        },
+        [setSearchQuery]
+    );
+
+    return (
+        <>
+            <Search value={searchQuery} onChange={setSearchQuery} />
+            {Boolean(uniqueTags.length) && <TagsList tags={uniqueTags} onTagClick={onTagClick} />}
+            {Boolean(filteredNotes.length) && <NotesList notes={filteredNotes} setNotes={setNotes} />}
+        </>
+    );
+}
+
 /*
  * Main - это отдельный компонент. React позволяет создавать отдельные компоненты в одном файле.
  * Иногда так делают, если не хотят выносить близкий по смыслу компонент в отдельный файл.
  */
 function Main() {
     const { notes, setNotes } = useContext(NotesContext);
-    const [searchQuery, setSearchQuery] = useState("");
-    const onAddNote = (note) => {
-        setNotes([...notes, note]); // Спред оператор.
-    };
 
-    const filteredNotes = notes.filter(
-        (note) => note.title.toLowerCase().includes(searchQuery.toLowerCase()) || note.tags.includes(searchQuery)
+    const onAddNote = useCallback(
+        (note) => {
+            setNotes([...notes, note]); // Спред оператор.
+        },
+        [notes, setNotes]
     );
-    const uniqueTags = [...new Set(notes.reduce((acc, note) => acc.concat(note.tags), []))];
-
-    const onTagClick = (tag) => {
-        setSearchQuery(tag);
-    };
 
     return (
         <div className={styles.app}>
             <div className={styles.container}>
                 <Header notesCount={notes.length} />
                 <NotesForm onAddNote={onAddNote} />
-                <Search value={searchQuery} onChange={setSearchQuery} />
-                {Boolean(uniqueTags.length) && <TagsList tags={uniqueTags} onTagClick={onTagClick} />}
-                {Boolean(filteredNotes.length) && <NotesList notes={filteredNotes} setNotes={setNotes} />}
+                <Notes />
             </div>
         </div>
     );
